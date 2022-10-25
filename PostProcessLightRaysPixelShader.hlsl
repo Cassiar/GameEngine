88f551fbf34e1442b00e27ba7e9f83c10610e8d0
Noah Shields <48223279+lakenoah17@users.noreply.github.com>
2022-10-25 13:00:17 -0400

    Cache commit
    Don't need these but conflicting merge for some reason

---
+++ PostProcessLightRaysPixelShader.hlsl
@@ -0,0 +1,64 @@
+//@source https://developer.nvidia.com/gpugems/gpugems3/part-ii-light-and-shadows/chapter-13-volumetric-light-scattering-post-process
+//@purpose post process shader to handle drawing light rays
+//made for IGME 550 game engine design and development
+
+#include "Structs.hlsli"
+
+#define NUM_SAMPLES 128
+//const float Density = 1.0;
+//const float Weight = 1.0;
+
+cbuffer PPLightRaysBuf : register(b0) {
+	float3 lightColor;
+	float density;
+	float weight;
+	float decay;
+	float exposure;
+}
+
+Texture2D ScreenTexture : register(t0); //screen image
+//Texture2D ShadowMap : register(t1);
+
+SamplerState BasicSampler : register(s0); //basic sampler
+//SamplerComparisonState ShadowSampler : register(s1);
+
+float4 main(VertexToPixel_PPLightRays input) : SV_TARGET
+{   
+	input.texCoord = clamp(input.texCoord, 0, 1);//clamp x and y to [0,1]
+	// Calculate vector from pixel to light source in screen space.    
+	//float2 deltaTexCoord = input.lightScreenPos;//currently passing in direction
+	float2 deltaTexCoord = (input.texCoord - input.lightScreenPos);
+
+	// Divide by number of samples and scale by control factor.   
+	deltaTexCoord *= 1.0f / NUM_SAMPLES * density;
+	
+	// Store initial sample.    
+	float3 color = ScreenTexture.Sample(BasicSampler, input.texCoord).xyz;
+	//return float4(color.rgb,1);
+	//return float4(input.texCoord.xy, 0,1);
+
+	// Set up illumination decay factor.    
+	float illuminationDecay = 1.0f;   
+
+
+	// Evaluate summation from Equation 3 NUM_SAMPLES iterations.    
+	for (int i = 0; i < NUM_SAMPLES; i++)   {     
+		// Step sample location along ray.     
+		input.texCoord -= deltaTexCoord;     
+		
+		// Retrieve sample at new location.    
+		float3 newColor = ScreenTexture.Sample(BasicSampler, input.texCoord).xyz;
+		//float3 newColor = lightColor.rgb;
+		// Apply sample attenuation scale/decay factors.     
+		newColor *= illuminationDecay * weight;
+		
+		// Accumulate combined color.     
+		color += newColor;
+		
+		// Update exponential decay factor.     
+		illuminationDecay *= decay;   
+	}   
+	
+	// Output final color with a further scale control factor.    
+	return float4( color * exposure, 1);
+} 
---
+++ PostProcessLightRaysVertexShader.hlsl
@@ -0,0 +1,51 @@
+//@author Cassiar Beaver cdb7951
+//@purpose convert a triangle view of the screen into usuable coordinates
+
+#include "Structs.hlsli"
+
+//constant buffer. Can't be changed by shaders but
+// can be changed from C++
+//world proj and view are from light's perspective
+cbuffer ExternalData : register(b0) {
+	matrix world;
+	//matrix worldInvTranspose;
+	matrix proj;
+	matrix view;
+	float3 lightPos;
+}
+
+VertexToPixel_PPLightRays main(uint id : SV_VERTEXID)
+{
+	VertexToPixel_PPLightRays output;
+
+	//convert light world pos to screen pos
+	//matrix wvp = mul(mul(proj, view), world);
+	//output.lightScreenPos = mul(wvp, float4(lightPos, 1.0f));
+	//output.lightScreenPos = mul(wvp, float4(1.0f, 1.0f, 1.0f, 1.0f));
+	
+	float4 tempPos = mul(mul(proj, view), float4(lightPos, 1.0f));
+	output.lightScreenPos = tempPos.xy / tempPos.w;
+
+	output.lightScreenPos = output.lightScreenPos * 0.5f + 0.5f;
+	output.lightScreenPos.y = 1 - output.lightScreenPos.y;
+	//output.lightScreenPos = float2(0.5f, 0.5f);
+
+	//============================
+	// Below code Author: Chris Cascioli
+	//============================
+	
+	// Calculate the UV (0,0 to 2,2) via the ID
+	// x = 0, 2, 0, 2, etc.
+	// y = 0, 0, 2, 2, etc.
+	output.texCoord = float2((id << 1) & 2, id & 2);
+
+	// Convert uv to the (-1,1 to 3,-3) range for position
+	output.position = float4(output.texCoord, 0, 1);
+	output.position.x = output.position.x * 2 - 1;
+	output.position.y = output.position.y * -2 + 1;
+
+	//Removes VS warning. Might be able to remove variable from struct entirely
+	output.shadowPos = float4(0.0f,0.0f,0.0f,0.0f);//mul(wvp, output.position);
+
+	return output;
+}
